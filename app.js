a5ea083be5c08d6d9778540875d792711cfa9703
steven <steven69700@gmail.com>
2023-08-24 17:09:46 +0200

    mise en place des fonctionnalité essentielle de la bdd

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+const cardRoute = require('./Routes/card')
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yugidata.dbmumtw.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
+  app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+  app.use('/cards', cardRoute)
+
 
 module.exports = app;
